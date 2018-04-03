574264a0434273d5100d965d4367cd9f6612329b
Keila Hoover <keila.hoover@gmail.com>
2018-04-03 10:01:15 -0600

    Add PATCH and DELETE for books

--- routes/books.js
+++ routes/books.js
@@ -51,4 +51,45 @@ router.post('/', (req, res, next) => {
     .catch((err) => console.log(err))
 })
 
+router.patch('/:id', (req, res, next) => {
+  const { id } = req.params
+  const { title, author, genre, description, coverUrl } = req.body
+  const cover_url = humps.decamelizeKeys(coverUrl)
+  knex('books')
+    .update({ title, author, genre, description, cover_url })
+    .where('id', id)
+    .returning('*')
+    .then((rows) => {
+      res.json(humps.camelizeKeys(rows[0]))
+    })
+    .catch((err) => console.log(err))
+})
+
+router.delete('/:id', (req, res, next) => {
+  const { id } = req.params
+  knex('books')
+  .del()
+  .where('id', id)
+  .returning(['title', 'author', 'genre', 'description', 'cover_url'])
+  .then((rows) => {
+    res.json(humps.camelizeKeys(rows[0]))
+  })
+  .catch((err) => console.log(err))
+})
+
+
+// router.delete('/:id', (req, res, next) => {
+//   const { id } = req.params
+//
+//   knex(bookTable)
+//     .del()
+//     .where('id', id)
+//     .returning('*')
+//     .then((rows) => {
+//       res.type('json')
+//       res.json(humps.camelizeKeys(rows[0]))
+//     })
+//     .catch((err) => console.log(err))
+// })
+
 module.exports = router;
