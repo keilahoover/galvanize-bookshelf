00fb3845cfe8c11b2b4e49794ceb248e48902f83
Keila Hoover <keila.hoover@gmail.com>
2018-04-03 08:29:06 -0600

    Add books migration and seed

---
+++ migrations/20180402211420_books.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('books', (table) => {
+    table.increments()
+    table.varchar('title', 255).notNullable().defaultTo('')
+    table.varchar('author', 255).notNullable().defaultTo('')
+    table.varchar('genre', 255).notNullable().defaultTo('')
+    table.text('description').notNullable().defaultTo('')
+    table.text('cover_url').notNullable().defaultTo('')
+    table.dateTime('created_at').notNullable().defaultTo(knex.raw('now()'))
+    table.dateTime('updated_at').notNullable().defaultTo(knex.raw('now()'))
+  })
+};
+
+exports.down = (knex, Promise) => {
+  return knex.schema.dropTableIfExists('books')
+}
