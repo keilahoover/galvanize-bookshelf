ad2f6d6938146b76f148b832b85aad9f54269ffb
Keila Hoover <keila.hoover@gmail.com>
2018-04-05 09:47:56 -0600

    Add user route

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,33 @@
 'use strict';
 
-const express = require('express');
+const express = require('express')
+const router = express.Router()
+const knex = require('../knex')
+const humps = require('humps')
+const bcrypt = require('bcrypt')
+const saltRounds = 10
 
-// eslint-disable-next-line new-cap
-const router = express.Router();
+router.post('/', (req, res, next) => {
+  const { firstName, lastName, email, password } = req.body
+    bcrypt.hash(password, saltRounds, (err, hash) => {
+      console.log(err);
+      const first_name = humps.decamelizeKeys(firstName)
+      const last_name = humps.decamelizeKeys(lastName)
+      const hashed_password = hash
+      knex('users')
+      .insert({
+        first_name, last_name, email, hashed_password
+      })
+      .returning(['first_name', 'last_name', 'email', 'id'])
+      .then((rows) => {
+        if (rows.length > 0) {
+          res.json(humps.camelizeKeys(rows[0]))
+        } else {
+          res.sendStatus(404)
+        }
+      })
+      .catch((err) => console.log(err))
+    })
+})
 
-// YOUR CODE HERE
-
-module.exports = router;
+module.exports = router
